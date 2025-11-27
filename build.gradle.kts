dbd5875eb1893e30ea87a6fe01db64e99298e5c4
Liu Rui <lovearuis@gmail.com>
2025-11-27 10:02:06 +0800

    version: 2.26.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 allprojects {
     group = "net.ximatai.muyun.database"
 //    version = "1.0.0-SNAPSHOT"
-    version = "2.26.5"
+    version = "2.26.6"
 
     repositories {
         maven { url = uri("https://mirrors.cloud.tencent.com/repository/maven") }
